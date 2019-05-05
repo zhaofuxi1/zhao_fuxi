fb1a3ebb5d0eaeef8b17ba2b1781dc5cd591e3ec
zhaofuxi11 <2014920717@qq.com>
2019-05-05 08:38:12 +0800

    测绘四班赵福玺作业

--- 8_10/main.cpp
+++ 8_10/main.cpp
@@ -3,8 +3,17 @@
 using namespace std;
 
 
-void zero(int bigIntegers[]);
-void zero(int &bigIntegers);
+void zero(int bigIntegers[])
+{
+
+}
+void zero(const int bigIntegers);
+int addlandsum(int onetoosmall[])
+{
+
+}
+void addlandsum(const int addlandsum());
+
 int main()
 {
     cout << "Hello world!" << endl;
